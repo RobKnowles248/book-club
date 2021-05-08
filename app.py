2bc0645e1b16c6ea9de7e37e554a29854e300980
RobKnowles248 <robert@knowles-family.me.uk>
2021-05-08 15:40:34 +0000

    Turn debug mode off

--- app.py
+++ app.py
@@ -283,4 +283,4 @@ def book_page(book_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
